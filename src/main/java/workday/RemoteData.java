924605d8f243f38e931e80af562de4f99a67484c
baofeng.xue <baofeng.xue@workday.com>
2021-11-04 22:33:50 -0700

    r

--- src/main/java/workday/RemoteData.java
+++ src/main/java/workday/RemoteData.java
@@ -2,32 +2,32 @@ package workday;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.IntSummaryStatistics;
-import java.util.Set;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 public class RemoteData {
-/*
-Statistics of time cost for remote data: count[125118], min[114], max[17408], avg[388.259385665529], median[261] (edited)
-Before the pool patch:
-Statistics of remote data: count[8860], min[114], max[19521], avg[2191.4144353899883], median[2194]
-* */
+
+    /*
+    Statistics of time cost for remote data: count[125118], min[114], max[17408], avg[388.259385665529], median[261]
+    Before the pool patch:
+    Statistics of remote data: count[8860], min[114], max[19521], avg[2191.4144353899883], median[2194]
+    * */
     public static void main(String[] args)
         throws Exception {
 
-        Set<Integer> totals = new HashSet<>();
-        Set<Integer> fetches = new HashSet<>();
-        Set<Integer> diffs = new HashSet<>();
+        List<Integer> totals = new ArrayList<>();
+        List<Integer> fetches = new ArrayList<>();
 
         final AtomicInteger sum = new AtomicInteger();
 
-        Files.walk(Paths.get("/Users/baofeng.xue/Downloads/_tomcat.oms.ots_2021.35.129_logs (1)"))
+        //        Files.walk(Paths.get("/Users/baofeng.xue/Downloads/_tomcat.oms.ots_2021.46.110_logs"))
+        //        Files.walk(Paths.get("/Users/baofeng.xue/Downloads/_tomcat.oms.ots_2021.35.129_logs (1)"))
+        Files.walk(Paths.get("/Users/baofeng.xue/Downloads/remote_data 2.log"))
             .filter(p -> p.toString().contains("remote_data"))
             .forEach(
                 f -> {
@@ -39,16 +39,21 @@ Statistics of remote data: count[8860], min[114], max[19521], avg[2191.414435389
                         e.printStackTrace();
                     }
 
-                    lines.filter(l -> l.contains("watsdb.jira"))
+                    lines.filter(l -> l.contains("WATS.jira") || l.contains("watsdb.jira"))
                         .forEach(
                             l -> {
                                 sum.getAndIncrement();
-                                String[] split = l.split("watsdb.jira,")[1].split(",");
+                                String[] split;
+                                if (l.contains("watsdb.jira")) {
+                                    split = l.split("watsdb.jira,")[1].split(",");
+                                }
+                                else {
+                                    split = l.split("WATS.jira,")[1].split(",");
+                                }
                                 int v1 = Integer.parseInt(split[1]);
                                 totals.add(v1);
                                 int v2 = Integer.parseInt(split[2]);
                                 fetches.add(v2);
-                                diffs.add(v1 - v2);
                             }
                                 );
                 }
@@ -56,21 +61,25 @@ Statistics of remote data: count[8860], min[114], max[19521], avg[2191.414435389
 
         IntSummaryStatistics totalS = totals.stream().mapToInt(Integer::valueOf).summaryStatistics();
         IntSummaryStatistics fetchesStat = fetches.stream().mapToInt(Integer::valueOf).summaryStatistics();
-        IntSummaryStatistics diffsStat = diffs.stream().mapToInt(Integer::valueOf).summaryStatistics();
-        System.out.println(fetchesStat);
-        System.out.println(diffsStat);
-        System.out.println(String.format("Statistics of remote data: count[%s], min[%s], max[%s], avg[%s], median[%s]",
+        String format = "Statistics of total: count[%s], min[%s], max[%s], avg[%.1f], median[%s]";
+        System.out.println(String.format(format,
             sum.get(),
             totalS.getMin(),
             totalS.getMax(),
             totalS.getAverage(),
             median(totals)
-            ));
+                                        ));
+        System.out.println(String.format(format,
+            sum.get(),
+            fetchesStat.getMin(),
+            fetchesStat.getMax(),
+            fetchesStat.getAverage(),
+            median(fetches)
+                                        ));
     }
 
-    private static int median(final Set<Integer> durations) {
-        ArrayList<Integer> integers = new ArrayList<>(durations);
-        Collections.sort(integers);
-        return integers.get(integers.size() / 2);
+    private static int median(final List<Integer> durations) {
+        Collections.sort(durations);
+        return durations.get(durations.size() / 2);
     }
 }
